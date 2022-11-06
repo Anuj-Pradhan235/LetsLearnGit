f684ad8d94a48da1d46a00151cf669055b0c1be1
Anuj Pradhan <pradhananuj235@gmail.com>
2022-11-06 18:30:43 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int num1;
+    int num2;
+    cout << "Enter the two numbers";
+    cin >> num1 >> num2;
+    int sum = num1 + num2;
+    cout << sum;
+}
