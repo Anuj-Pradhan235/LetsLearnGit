51191a9b58b4c65a803bb2ec91ebdf548e1d6c0d
Anuj Pradhan <pradhananuj235@gmail.com>
2022-11-06 19:41:55 +0530

    Changes in the Multiply.cpp

---
+++ Multiply.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int num1;
+    int num2;
+    cout << "Eter te two numbers";
+    cin >> num1 >> num2;
+    int sum = num1 + num2;
+    cout << "This is the sum of two digits" << sum;
+}
